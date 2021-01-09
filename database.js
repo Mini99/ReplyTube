21417c7d7f8f60f0410b26c77b847a67982f3549
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-01-09 14:18:41 -0800

    moved database file to app

--- database.js
+++ database.js
@@ -1,23 +1,23 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-class Database {
+// class Database {
 
-    constructor() {
-        this.connect();
-    }
+//     constructor() {
+//         this.connect();
+//     }
 
-    connect() {
-        const connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'replytube'
-        });
-        connection.connect((err) => {
-            if (err) throw err;
-            console.log('Connected to MySQL Server!');
-        });
-    }
-}
+//     connect() {
+//         const connection = mysql.createConnection({
+//             host: 'localhost',
+//             user: 'root',
+//             password: '',
+//             database: 'replytube'
+//         });
+//         connection.connect((err) => {
+//             if (err) throw err;
+//             console.log('Connected to MySQL Server!');
+//         });
+//     }
+// }
 
-module.exports = new Database();
+// module.exports = new Database();
