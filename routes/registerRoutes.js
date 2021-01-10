75b4ff0feaec0e218811fb6830caca0db4347773
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-01-09 17:44:57 -0800

    implemented a check for username/password

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
-const User = require('../query');
 const mysql = require('mysql');
 
 app.set("view engine", "pug");
@@ -16,29 +15,50 @@ router.get("/", (req, res, next) => {
     res.status(200).render("register");
 })
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
 
-    var firstName = req.body.firstName.trim();
-    var lastName = req.body.lastName.trim();
     var username = req.body.username.trim();
     var email = req.body.email.trim();
     var password = req.body.password;
 
     var payload = req.body;
 
-    if(firstName && lastName && username && email && password) {
+    if(username && email && password) {
+        //use db class instead
         const con = mysql.createConnection({
             host: process.env.DATABASE_HOST,
             user: process.env.DATABASE_USER,
             password: process.env.DATABASE_PASSWORD,
             database: process.env.DATABASE
         });
-        
-        var sql = "INSERT INTO users (username, email, password) VALUES (?,?,?)";
-        con.query(sql, [username, email, password], function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
+
+        var check = "SELECT * FROM users WHERE username=? or email=?";
+        var user = await con.query(check, [username, email], function (err, result) {
+            if (result.length != 0){
+                return 1;
+            }
         });
+
+        if(user){
+            var sql = "INSERT INTO users (username, email, password) VALUES (?,?,?)";
+            con.query(sql, [username, email, password], function (err, result) {
+            if (err) throw err;
+                console.log("1 record inserted");
+            });
+
+            res.status(200).render("login");
+        }
+        else {
+            if(email == user.email) {
+                payload.errorMessage = "Email already in use.";
+            }
+            else {
+                payload.errorMessage = "Username already in use.";
+            }
+            res.status(200).render("register", payload);
+        }
+        
+        // make a close connection statement
     }
     else {
         payload.errorMessage = "Make sure each field has a valid value.";
