29d264f6941f1ced78141480386b3b4b7e0e8fc9
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-01-12 18:09:11 -0800

    initial push

--- app.js
+++ app.js
@@ -14,17 +14,6 @@ const database = require('./database')
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
-// const connection = mysql.createConnection({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE
-// });
-// connection.connect((err) => {
-//     if (err) throw err;
-//     console.log('Connected to MySQL Server!');
-// });
-
 app.set("view engine", "pug");
 app.set("views", "views");
 
