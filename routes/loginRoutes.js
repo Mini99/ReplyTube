ecbc40ff53a06fe106a53f3f7d38e949ce3999c2
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-02-25 20:06:24 -0800

    made a login route

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,22 +1,58 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const router = express.Router();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const mysql = require('mysql')
 const bcrypt = require('bcrypt');
+const path = require('path')
 
-app.set("view engine", "pug");
+app.set('view-engine', 'pug')
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded( { extended: false }));
 
 router.get("/", (req, res, next) => {
 
     res.status(200).render("login");
 })
 
-router.post("/", async (req, res, next) => {
+router.post("/", (req, res, next) => {
 
-    
+    var user = {
+        username: req.body.logUsername,
+        password: req.body.logPassword,
+    }
+
+    if(req.body.logEmail && req.body.logPassword) {
+        const con = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_DATABASE
+        });
+
+        con.query("SELECT * FROM users WHERE email='"+ req.body.logUsername +"'", async function(err, result, field){
+            if(result.length > 0){
+                var comp = await bcrypt.compare(req.body.logPassword, result[0].password);
+                if(comp === true) {
+                    req.session.user = user;
+                    return res.redirect("/");
+                }
+                else {
+                    var errorMessage = "Incorrect Password.";
+                    return res.status(200).render("login", {errorMessage});
+                }
+            }
+            else {
+                var errorMessage = "User does not exist.";
+                return res.status(200).render("login", {errorMessage});
+            }
+        });
+    }
+    else {
+        return res.redirect("login");
+    }
 })
 
 module.exports = router;
