b591b8dc83de1197b023bf6b5fc0295f6f02ba7d
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-02-26 23:11:27 -0800

    able to output posts from database for each video

--- public/js/urlPage.js
+++ public/js/urlPage.js
@@ -1,3 +1,11 @@
+$(document).ready(() => {
+    var urlId = window.location.href.slice(-11);
+
+    $.get("/api/posts/" + urlId, results => {
+        outputPosts(results, $(".commentsContainer"))
+    })
+})
+
 $("#commentTextarea").keyup(event => {
     var textbox = $(event.target);
     var value = textbox.val().trim();
@@ -14,4 +22,56 @@ $("#commentTextarea").keyup(event => {
         submitButton.prop("disabled", false);
     }
 })
+
+$("#submitPostButton").click(() => {
+    var button = $(event.target);
+    var textbox = $("#commentTextarea");
+
+    var urlId = window.location.href.slice(-11);
+
+    var data = {
+        content: textbox.val(),
+        urlId: urlId
+    }
+
+    $.post("/api/posts", data, (postData, status, xhr) => {
+        var html = createCommentHtml(postData);
+        $(".commentsContainer").prepend(html);
+        textbox.val("");
+        button.prop("disabled", true);
+    })
+})
+
+function createCommentHtml(postData) {   
+    
+    return `<div class='post' data-id=${postData.postId}>
+                <div class='mainPostContentContainer'>
+                    <div class='postImageContainer'>
+                        <img src=''>
+                    </div>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
+
+function outputPosts(results, container) {
+    container.html("");
+
+    results.forEach(result => {
+        var html = createCommentHtml(result)
+        container.prepend(html);
+    });
+
+    if(results.length == 0) {
+        container.prepend("<span class='noResults'>Nothing to show</span>")
+    }
+}
 
