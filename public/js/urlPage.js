c77455df10cdda6a3f544f310177d8d4db4f5976
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-02-26 16:54:38 -0800

    enables post button on click

---
+++ public/js/urlPage.js
@@ -0,0 +1,17 @@
+$("#commentTextarea").keyup(event => {
+    var textbox = $(event.target);
+    var value = textbox.val().trim();
+
+    var submitButton = $("#submitPostButton");
+
+    if(submitButton.length == 0) return alert("No submit button found");
+
+    if(value == "") {
+        submitButton.prop("disabled", true);
+        return;
+    }
+    else {
+        submitButton.prop("disabled", false);
+    }
+})
+
