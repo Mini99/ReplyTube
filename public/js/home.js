71b98f88188540f811647240d3379c6798f2804c
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-09-05 23:07:15 -0700

    fixed issue where additonal vidoes were requested

--- public/js/home.js
+++ public/js/home.js
@@ -9,24 +9,23 @@ var end = 19;
 
 $(window).scroll(function() {
     $.get("/api/urls/countVids", results => {
+        console.log(results.countVids);
         if(end <= results.countVids) {
             if($(window).scrollTop() == $(document).height() - $(window).height()) {
                 document.getElementById("loadWheel").style.display = "block";
-                setTimeout(() => { 
-                    $.ajax({
-                        url: "/api/urls/range/" + start + "/" + end,
-                        type: "GET",
-                        success: (results) => {
-                            document.getElementById("loadWheel").style.display = "none";
-                            results.forEach(result => {
-                                var html = createPostHtml(result)
-                                $(".urlsContainer").append(html).fadeIn();
-                            });
-                        }
-                    })
-                    start+=10;
-                    end+=10;
-                }, 1000);
+                $.ajax({
+                    url: "/api/urls/range/" + start + "/" + end,
+                    type: "GET",
+                    success: (results) => {
+                        document.getElementById("loadWheel").style.display = "none";
+                        results.forEach(result => {
+                            var html = createPostHtml(result)
+                            $(".urlsContainer").append(html);
+                        });
+                    }
+                })
+                start+=10;
+                end+=10;
             }
         }
     })
