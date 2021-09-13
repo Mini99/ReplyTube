883a32141e9347e0e5d61bae39d904c0ffdfe937
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-09-13 13:04:16 -0700

    optimized scroll to load function

--- public/js/home.js
+++ public/js/home.js
@@ -7,11 +7,33 @@ $(document).ready(() => {
 var start = 10;
 var end = 19;
 
+// $(window).scroll(function() {
+//     $.get("/api/urls/countVids", results => {
+//         if(end <= results.countVids) {
+//             if($(window).scrollTop() == $(document).height() - $(window).height()) {
+//                 document.getElementById("loadWheel").style.display = "block";
+//                 $.ajax({
+//                     url: "/api/urls/range/" + start + "/" + end,
+//                     type: "GET",
+//                     success: (results) => {
+//                         document.getElementById("loadWheel").style.display = "none";
+//                         results.forEach(result => {
+//                             var html = createPostHtml(result)
+//                             $(".urlsContainer").append(html);
+//                         });
+//                     }
+//                 })
+//                 start+=10;
+//                 end+=10;
+//             }
+//         }
+//     })
+// });
+
 $(window).scroll(function() {
-    $.get("/api/urls/countVids", results => {
-        console.log(results.countVids);
-        if(end <= results.countVids) {
-            if($(window).scrollTop() == $(document).height() - $(window).height()) {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+        $.get("/api/urls/countVids", results => {
+            if(end <= results.countVids) {
                 document.getElementById("loadWheel").style.display = "block";
                 $.ajax({
                     url: "/api/urls/range/" + start + "/" + end,
@@ -27,6 +49,6 @@ $(window).scroll(function() {
                 start+=10;
                 end+=10;
             }
-        }
-    })
+        })
+    }
 });
