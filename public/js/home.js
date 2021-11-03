f0fb2a93fb35364725a1d1556c283875af575052
Manvir Dhillon <manvir.dhillon30@gmail.com>
2021-11-02 20:10:42 -0700

    removed unused code

--- public/js/home.js
+++ public/js/home.js
@@ -7,29 +7,6 @@ $(document).ready(() => {
 var start = 10;
 var count = 10;
 
-// $(window).scroll(function() {
-//     $.get("/api/urls/countVids", results => {
-//         if(end <= results.countVids) {
-//             if($(window).scrollTop() == $(document).height() - $(window).height()) {
-//                 document.getElementById("loadWheel").style.display = "block";
-//                 $.ajax({
-//                     url: "/api/urls/range/" + start + "/" + end,
-//                     type: "GET",
-//                     success: (results) => {
-//                         document.getElementById("loadWheel").style.display = "none";
-//                         results.forEach(result => {
-//                             var html = createPostHtml(result)
-//                             $(".urlsContainer").append(html);
-//                         });
-//                     }
-//                 })
-//                 start+=10;
-//                 end+=10;
-//             }
-//         }
-//     })
-// });
-
 $(window).scroll(function() {
     if($(window).scrollTop() == $(document).height() - $(window).height() && count > 0) {
         $.get("/api/urls/countVids", results => {
